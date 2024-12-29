137333a65e391c98c2d4ec301555b611c500fc43
Goldfinch <facu_vila@hotmail.com>
2024-12-29 04:05:05 -0300

    añadiendo el modal de ingresar

--- src/app/components/modal-ingresar/modal-ingresar.component.ts
+++ src/app/components/modal-ingresar/modal-ingresar.component.ts
@@ -1,11 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal-ingresar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './modal-ingresar.component.html',
   styleUrl: './modal-ingresar.component.scss'
 })
 export class ModalIngresarComponent {
+  showModal: boolean = false;
 
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
